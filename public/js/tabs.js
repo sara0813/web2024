aa98706b6702571eadc2993a564a9155865aa63c
sara <msaram0813@gmail.com>
2024-11-04 14:26:14 +0900

    Initial commit

---
+++ public/js/tabs.js
@@ -0,0 +1,17 @@
+function f1() {
+	location.href = '../html/register.html';
+	}
+
+function f2() {
+	location.href = '../html/search.html';
+	}
+
+function f3() {
+	location.href = '../html/messages.html';
+	}
+
+function f4() {
+	location.href = '../html/profile.html';
+	}
+
+document.oncontextmenu = function(){return false;}
