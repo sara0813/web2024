b0cb3665c8e079e64e0ed45e8030cf9d41e2e0b7
sara <msaram0813@gmail.com>
2024-11-08 01:21:59 +0900

    경로 문제 해결

--- js/tabs.js
+++ js/tabs.js
@@ -1,33 +1,33 @@
 function f1() {
-	location.href = '/html/register.html';
+	location.href = 'register.html';
 	}
 
 function f2() {
-	location.href = '/html/search.html';
+	location.href = 'search.html';
 	}
 
 function f3() {
-	location.href = '/html/messages.html';
+	location.href = 'messages.html';
 	}
 
 function f4() {
-	location.href = '/html/profile.html';
+	location.href = 'profile.html';
 	}
 
 function f1a() {
-    location.href = '/html/register.html';
+    location.href = 'html/register.html';
 }
 
 function f2a() {
-    location.href = '/html/search.html';
+    location.href = 'html/search.html';
 }
 
 function f3a() {
-    location.href = '/html/messages.html';
+    location.href = 'html/messages.html';
 }
 
 function f4a() {
-    location.href = '/html/profile.html';
+    location.href = 'html/profile.html';
 }
 
 document.oncontextmenu = function(){return false;}
