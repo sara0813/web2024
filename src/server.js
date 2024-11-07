718a9740d93f9bf1e0af9c370f2601cb3e20261e
sara <msaram0813@gmail.com>
2024-11-08 01:53:20 +0900

    시도중

--- src/server.js
+++ src/server.js
@@ -3,12 +3,12 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-// 정적 파일 제공 (css, js, html 폴더에 접근)
-app.use(express.static(path.join(__dirname, '../'))); // 최상위 폴더의 파일들 제공
+// 정적 파일 제공 (css, js 폴더에 접근)
+app.use(express.static(path.join(__dirname, '..'))); // 루트 디렉토리로 이동하여 모든 파일 제공
 
 // 기본 라우트 설정
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../index.html')); // 최상위 폴더의 index.html
+    res.sendFile(path.join(__dirname, '..', 'index.html')); // 루트 디렉토리의 index.html
 });
 
 // 서버 시작
