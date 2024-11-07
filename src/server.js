15b2d38c59d5b2e8315cb41faf6e580fed15106e
sara <msaram0813@gmail.com>
2024-11-08 00:41:23 +0900

    경로 수정

--- src/server.js
+++ src/server.js
@@ -1,14 +1,18 @@
-const express = require('express'); 
-const path = require('path'); // path 모듈을 추가합니다.
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon'); // serve-favicon 패키지 추가
 const app = express();
 const port = 3000;
 
-// 정적 파일 제공 (HTML, CSS, JS 파일 등)
-app.use(express.static(path.join(__dirname, '../public'))); // 정적 파일 경로 수정
+// favicon 경로 설정
+app.use(favicon(path.join(__dirname, '../public', 'favicon.ico')));
+
+// 정적 파일 제공
+app.use(express.static(path.join(__dirname, '../public')));
 
 // 기본 라우트 설정
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../index.html')); // 메인 페이지 경로 수정
+    res.sendFile(path.join(__dirname, '../index.html'));
 });
 
 // 서버 시작
