ab6ff6a280603b1c18bb7995f4ca8c69f1dcf78f
sara <msaram0813@gmail.com>
2024-11-13 00:20:34 +0900

    이메일 인증 번호 전송 관련 오류 해결

--- js/signup/sendVerificationCode.js
+++ js/signup/sendVerificationCode.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function sendVerificationCode() {
     const email = document.getElementById("email").value;
     
@@ -24,27 +23,3 @@ function sendVerificationCode() {
         alert("서버와의 연결에 실패하였습니다. 다시 시도해 주세요.");  // 네트워크 오류시 경고창
     });
 }
-=======
-// 이메일 인증 코드 발송 함수
-function sendVerificationCode() {
-    const email = document.getElementById('email').value;
-    if (validateEmail(email)) {
-        fetch('/send-verification-email', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ email: email })
-        }).then(response => response.json())
-          .then(data => {
-              if (data.success) {
-                  alert('인증번호가 발송되었습니다.');
-              } else {
-                  alert('이메일 전송에 실패했습니다.');
-              }
-          }).catch(error => {
-              console.error('Error:', error);
-          });
-    } else {
-        alert('올바른 이메일 주소를 입력해주세요.');
-    }
-}
->>>>>>> 239f43a3d7c58df2d56bc5785677f0591018c748
